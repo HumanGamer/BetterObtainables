e8655d3d886f96542b4659555ef0065fa425372b
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2019-03-15 14:22:05 -0400

    Initial Commit.

---
+++ BetterObtainables.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BetterObtainables
+{
+	public class BetterObtainables : Mod
+	{
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(this);
+			recipe.AddIngredient(ItemID.IronBar, 5);
+			recipe.anyIronBar = true;
+			recipe.AddIngredient(ItemID.Silk, 10);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(ItemID.IceSkates);
+			recipe.AddRecipe();
+
+			recipe = new ModRecipe(this);
+			recipe.AddIngredient(ItemID.GoldBar, 7);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(ItemID.Sextant);
+			recipe.AddRecipe();
+
+			recipe = new ModRecipe(this);
+			recipe.AddIngredient(ItemID.IronBar, 15);
+			recipe.anyIronBar = true;
+			recipe.AddIngredient(ItemID.Wire, 10);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(ItemID.WeatherRadio);
+			recipe.AddRecipe();
+			
+			recipe = new ModRecipe(this);
+			recipe.AddIngredient(ItemID.WaterWalkingPotion, 1);
+			recipe.AddIngredient(ItemID.Silk, 10);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(ItemID.WaterWalkingBoots);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ NPCStuff.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BetterObtainables
+{
+	public class NPCStuff : GlobalNPC
+	{
+		public override void SetupShop(int type, Chest shop, ref int nextSlot)
+		{
+			if (type == NPCID.Truffle)
+			{
+				shop.item[nextSlot].SetDefaults(ItemID.TruffleWorm);
+				shop.item[nextSlot].shopCustomPrice = Item.buyPrice(0, 15);
+				nextSlot++;
+			}
+		}
+
+		public override void SetupTravelShop(int[] shop, ref int nextSlot)
+		{
+			shop[nextSlot++] = ItemID.FishermansGuide;
+		}
+	}
+}
